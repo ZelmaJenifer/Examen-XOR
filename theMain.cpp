9d8c8c0ca08f6c2318a1d38296cb8239e8a23d52
Zelma J. Loera <zelma.loera@gmail.com>
2017-07-08 14:03:14 -0500

    theMain.cpp
    Main file.

---
+++ theMain.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Evaluacion_Lasec.cpp
+// Author      : Zelma J. Loera
+// Version     : 1
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+#include "claseXOR.h"
+using namespace std;
+
+int main() {
+	claseXOR* examen= new claseXOR();
+	examen->principal();
+
+	return 0;
+}
